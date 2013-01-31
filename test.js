22e6795e4bb6d93e1b6c4fb87b944b8d998b0e04
Michal Miskernik <michal.miskernik@gmail.com>
2013-01-31 12:20:03 +0100

    tests

---
+++ test.js
@@ -0,0 +1,42 @@
+var expect = require('expect.js');
+    Vector = require('./index');
+
+describe('Vector', function() {
+  function vec(x, y) {
+    return new Vector(x, y);
+  }
+
+  it('should create instances', function() {
+    var v1 = vec(3.5, 2.32),
+        v2 = vec(0, 8);
+
+    expect(String(v1)).to.be('(3.5;2.32)');
+    expect(String(v2)).to.be('(0;8)');
+  });
+
+  it('should do arithmetics', function() {
+    var v = vec(10, 4);
+
+    v
+      .nadd(2, 6)
+      .nsubtract(3.5, 0.5)
+      .multiply(2)
+      .divide(0.25);
+
+    expect(String(v)).to.be('(68;76)');
+  });
+
+  it('should do arithmetics with vectors', function() {
+    var v = vec(1, 2);
+
+    v
+      .vadd(vec(7, 2))
+      .vsubtract(vec(3, 4));
+
+    expect(String(v)).to.be('(5;0)');
+  });
+
+  it('should calculate length', function() {
+    expect(vec(3, 4).length()).to.be(5);
+  });
+});
